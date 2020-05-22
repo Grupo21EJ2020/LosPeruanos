3d34c3387ff731370422f8b6022e37a44861c679
SammyDeAztlan <silver.palacios@gmail.com>
2020-05-22 13:06:38 -0500

    avanze menu

--- source.py
+++ source.py
@@ -1,8 +1,34 @@
 #Codigo Chido para el pia osi osi 
+
+class Empleado():
+    def __init__(self,idEmpleado,Nombre,Direccion):
+        self.idEmpleado= idEmpleado
+        self.Nombre= Nombre
+        self.Direccion= Direccion
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
 print("»"*60)
+
 loop= True
 while loop == True:
-    accion=int(input("Dime que quieres hacer:\n")))
+    accion=int(input("Dime a que archivo deseas entrar:\n Curso=1:\n Empleado=2:\n Video=3:\n Tema=4:\n Curso_Tema=5:\n Curso_Tema_Video=6:\n")):
+        if accion == 2:
+            opcion = int(input("Dime que deseas hacer en este archivo:\n Agregar= 1:\n Borrar=2:\n Modificar=3:\n Consultar todo=4:\n Ver detalles de algun empleado=5:\n")):
+                if opcion == 1:
+                    agregado.empleado("Dame los datos del empleado")
+                 
 
 
     
