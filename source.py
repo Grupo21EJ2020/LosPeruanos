c6c54925dd673db8af48772fdf0ad3c28e478c33
SammyDeAztlan <silver.palacios@gmail.com>
2020-05-22 14:09:48 -0500

    cambios en el codigo

--- source.py
+++ source.py
@@ -5,7 +5,6 @@ class Empleado():
         self.idEmpleado= idEmpleado
         self.Nombre= Nombre
         self.Direccion= Direccion
-        
 
 
 
@@ -23,12 +22,21 @@ print("»"*60)
 
 loop= True
 while loop == True:
-    accion=int(input("Dime a que archivo deseas entrar:\n Curso=1:\n Empleado=2:\n Video=3:\n Tema=4:\n Curso_Tema=5:\n Curso_Tema_Video=6:\n")):
-        if accion == 2:
-            opcion = int(input("Dime que deseas hacer en este archivo:\n Agregar= 1:\n Borrar=2:\n Modificar=3:\n Consultar todo=4:\n Ver detalles de algun empleado=5:\n")):
-                if opcion == 1:
-                    agregado.empleado("Dame los datos del empleado")
-                 
+    accion=int(input("Dime a que archivo deseas entrar:\n Curso=1:\n Empleado=2:\n Video=3:\n Tema=4:\n Curso_Tema=5:\n Curso_Tema_Video=6:\n"))
+    if accion == 2:
+        opcion = int(input("Dime que deseas hacer en este archivo:\n Agregar= 1:\n Borrar=2:\n Modificar=3:\n Consultar todo=4:\n Ver detalles de algun empleado=5:\n"))
+        if opcion == 1:
+            nuevoempleado=Empleado
+            input("Dame el id del empleado"(nuevoempleado.idEmpleado))
+
+
+
+
+
+
+
+
+
 
 
     
