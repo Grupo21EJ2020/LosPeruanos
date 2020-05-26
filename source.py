5e4587a722ee884470b0515d069f22f3149ae376
Alejandro Rodriguez <alex.rdz.chavi@gmail.com>
2020-05-25 22:54:43 -0500

    Add agregar curso

--- source.py
+++ source.py
@@ -15,6 +15,9 @@ class Curso():
         self.descripcion = descripcion
         self.idEmpleado = idEmpleado
 
+   
+
+
     
 
 
@@ -34,6 +37,36 @@ print("»"*60)
 loop= True
 while loop == True:
     accion=int(input("Dime a que archivo deseas entrar:\n Curso=1:\n Empleado=2:\n Video=3:\n Tema=4:\n Curso_Tema=5:\n Curso_Tema_Video=6:\n"))
+    if accion == 1:
+         opcion = int(input("Dime que deseas hacer en este archivo:\n Agregar= 1:\n Borrar=2:\n Modificar=3:\n Consultar todo=4:\n Ver detalles de algun curso=5:\n"))
+         if opcion == 1:
+             archivo = open("./archivos/curso.txt","a",encoding='utf8')
+             idCurso = int(input("id de curso: \n"))  
+             descripcion = input("nombre de curso: \n")   
+             idEmpleado = int(input("Id de empleado: \n"))
+         
+             archivo.write( str(idCurso) + '!' + descripcion + '!' + str(idEmpleado))
+             archivo.write("\n")
+             
+
+        
+            
+             archivo.close
+         
+
+             
+        
+
+             
+             
+              
+             
+             
+             
+            
+            
+
+            
     if accion == 2:
         opcion = int(input("Dime que deseas hacer en este archivo:\n Agregar= 1:\n Borrar=2:\n Modificar=3:\n Consultar todo=4:\n Ver detalles de algun empleado=5:\n"))
         if opcion == 1:
