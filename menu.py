b420d39f01dfa29abccf508f6d39ee17f648b63f
SammyDeAztlan <silver.palacios@gmail.com>
2020-05-27 10:12:27 -0500

    se agrega empleado al menu.py

--- menu.py
+++ menu.py
@@ -56,7 +56,37 @@ while loop == True:
              linea = f.readline()
              print(linea)
              f.close()
+
+
+
+
+             
     if accion == 2:
+        opcion = int(input("Dime que deseas hacer en este archivo:\n Agregar= 1:\n Borrar=2:\n Modificar=3:\n Consultar todo=4:\n Ver detalles de algun empleado=5:\n"))
+        if opcion == 1:
+
+
+           #AGREGAR EMPLEADO OSI OSI
+            archivo = open("./archivos/empleados.txt","a",encoding='utf8')
+            idEmpleado = int(input("Dame el id del empleado crack: \n"))  
+            nombre = input("Dame el nombre: \n")   
+            direccion = (input("Dame la direccion del empleado: \n"))
+         
+            archivo.write( str(idEmpleado) + '!' + nombre + '!' + str(direccion))
+            archivo.write("\n")
+             
+
+        
+            
+            archivo.close
+        if opcion ==2:
+            #BORRAR DATOS DE EMPLEADOS UWU
+            listaborrado=[]
+            borrado=input("Dime que dato deseas borrar de este archivo: \n")
+            archivoread= open("./archivos/empleados.txt",encoding='utf8')
+            print(archivoread.read())
+            archivoread.close()
+        
 
 
 
