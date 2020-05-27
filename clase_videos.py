0d077880e89086105e5ca6d1a8cab8bf4a9af9d3
Daniel Alejandro Siller Garcia <danny.999@live.com.mx>
2020-05-26 22:11:37 -0500

    Add propiedades a claseVideos

--- clase_videos.py
+++ clase_videos.py
@@ -5,3 +5,18 @@ class Video:
         self.__nombre = nombre
         self.__url = url
         self.__fecha_publicacion
+
+    #Propiedades 
+    @property
+    def idvideo(self):
+        return self.__idvideo 
+    @property
+    def nombre(self):
+        return self.__nombre
+    @property
+    def url(self):
+        return self.__url
+    @property
+    def fecha_publicacion(self):
+        return self.__fecha_publicacion
+    
