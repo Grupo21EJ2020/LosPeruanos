b23886e8ed8d1bd2bef210b83fc55ab1940473fe
Daniel Alejandro Siller Garcia <danny.999@live.com.mx>
2020-05-26 21:42:24 -0500

    Add ClaseVideos

---
+++ clase_videos.py
@@ -0,0 +1,7 @@
+
+class Video:
+    def __init__(self,idvideo,nombre,url,fecha_publicacion):
+        self.__idvideo = idvideo
+        self.__nombre = nombre
+        self.__url = url
+        self.__fecha_publicacion
